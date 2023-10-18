8fb3b6b049dbaa0df5d68c5ae1535675a21b468e
Nik Polovenko <polovenko.nikita@gmail.com>
2023-10-18 15:52:34 +0200

    commit

---
+++ app/_layout.tsx
@@ -0,0 +1,83 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { useColorScheme } from "react-native";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  // Replace this with clerk key
+  if (process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY) {
+    return (
+      <ClerkProvider
+        publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      >
+        <RootLayoutNav />
+      </ClerkProvider>
+    );
+  } else {
+    return null;
+  }
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <SignedIn>
+          <Stack.Screen
+            name="(authenticated)"
+            options={{ headerShown: false }}
+          />
+        </SignedIn>
+        <SignedOut>
+          <Stack.Screen
+            name="(unauthenticated)"
+            options={{ headerShown: false }}
+          />
+        </SignedOut>
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+      </Stack>
+    </ThemeProvider>
+  );
+}
